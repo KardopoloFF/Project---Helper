26a4b4a1f62b14e7356144b84544480fb4f22598
Nikita Kardopolov <reverdj@yandex.ru>
2022-12-02 15:56:08 +0300

    some changes

--- client/src/components/Reg_Auth/Reg.tsx
+++ client/src/components/Reg_Auth/Reg.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useCallback, useState } from 'react'
+
+axios.defaults.baseURL = 'http://localhost:3001/'
 
 export default function Reg() {
+  const [img, setImg] = useState<any>(null)
+  const [avatar, setAvatar] = useState(null)
+
+  const sendFile = useCallback(async () => {
+    try {
+      const data = new FormData()
+      data.append('avatar', img)
+
+      await axios.post('api/upload', data, {
+        headers: {
+          'content-type': 'multipart/form-data'
+        }
+      })
+
+      .then(res => setAvatar(res.data.path))
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }, [img])
+
+
   return (
     <div>
-      <input type="file"/> 
-      <button type='submit'>send</button>
+      {avatar 
+        ? <img src={` ${avatar} `} alt="avatar" />
+        : <img src="" alt="avatar" />}
+      <input type="file" onChange={e => setImg(e.target.files[0])}/> 
+      <button type='submit' onClick={sendFile}>send</button>
     </div>
   )
 }
--- server/routes/uploadRouter.js
+++ server/routes/uploadRouter.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const path = require('path');
+const { User } = require('../db/models');
 
 const middlewaresUpload = require('../middlewares/file');
 
 const router = express.Router();
 
-router.post('/upload', middlewaresUpload.single('avatar'), (req, res) => {
+router.post('/upload', middlewaresUpload.single('avatar'), async (req, res) => {
   try {
     if (req.file) {
-      res.json(req.file);
+      const newImg = await User.create({ img: req?.file.path });
+      res.json(newImg);
     }
   } catch (error) {
     console.log(error);
