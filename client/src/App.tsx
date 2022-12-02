4ec45fb5bc83adca158d8ffd92793084f00c6ed2
Nikita Kardopolov <reverdj@yandex.ru>
2022-12-02 15:59:26 +0300

    some changes2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Reg from './components/Reg_Auth/Reg';
 
 function App() {
   return (
-    <>
     <Navbar />
-    <Reg/>
-    </>
   );
 }
 
