2aba04b8dfc02d53c7bf52ef0a51545d3b6d5c50
Aleksandr Proshkov <all9100@mail.ru>
2022-12-05 16:05:20 +0300

    filter fix

--- client/src/components/FindTask.tsx
+++ client/src/components/FindTask.tsx
@@ -2,25 +2,35 @@ import React, { useEffect,useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import OneTask from './OneTask'
 import {ITask} from '../types/task'
+import { ICategories } from '../types/categories';
 import {fetchPosts} from '../redux/postsSlice'
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Box from '@mui/material/Box';
 import { FormGroup, FormLabel } from '@mui/material'
+import { fetchCategories } from '../redux/categoriesSlice';
 
 interface Istore {
 store: {}
 posts: Array<ITask>
+categories: Array<ICategories>
 }
 
 export default function FindTask() {
     const tasks = useSelector((store:Istore)=> store.posts)
+    const categories = useSelector((store:Istore)=>store.categories)
     const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(fetchCategories())
+      console.log(categories);
+      
+    },[])
    
     const [state, setState] = useState({
         frst: true,
         scnd: false,
         thrd: false,
+        four: false,
+        five: false,
       });
 
       const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -29,7 +39,7 @@ export default function FindTask() {
           [event.target.name]: event.target.checked,
         });
       };
-      const { frst, scnd, thrd } = state;
+      const { frst, scnd, thrd, four, five } = state;
    
     useEffect(()=>{
         dispatch(fetchPosts(state))
@@ -46,19 +56,31 @@ export default function FindTask() {
             control={
               <Checkbox checked={frst} onChange={handleChange} name="frst" />
             }
-            label="frst category"
+            label="Бытовые услуги"
           />
           <FormControlLabel
             control={
               <Checkbox checked={scnd} onChange={handleChange} name="scnd" />
             }
-            label="scnd category "
+            label="Грузоперевозки"
           />
           <FormControlLabel
             control={
               <Checkbox checked={thrd} onChange={handleChange} name="thrd" />
             }
-            label="thrd category"
+            label="Курьерские услуги"
+          />
+            <FormControlLabel
+            control={
+              <Checkbox checked={four} onChange={handleChange} name="four" />
+            }
+            label="Компьютерная помощь"
+          />
+            <FormControlLabel
+            control={
+              <Checkbox checked={five} onChange={handleChange} name="five" />
+            }
+            label="Красота и здоровье"
           />
         </FormGroup>
     </div>
