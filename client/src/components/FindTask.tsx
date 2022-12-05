d85b01177e80fe9a1a3902b96b2cab1896a55335
Stepan Kirakosian <mailbox_35@mail.ru>
2022-12-05 16:30:04 +0300

    upd-findTask + map

--- client/src/components/FindTask.tsx
+++ client/src/components/FindTask.tsx
@@ -1,67 +1,87 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import OneTask from './OneTask'
-import {ITask} from '../types/task'
-import {fetchPosts} from '../redux//slices/postsSlice'
+import { ITask } from '../types/task'
+import { fetchPosts } from '../redux//slices/postsSlice'
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Box from '@mui/material/Box';
-import { FormGroup, FormLabel } from '@mui/material'
+import { Button, FormGroup, FormLabel } from '@mui/material'
+import PlaceIcon from '@mui/icons-material/Place';
+import { Link } from 'react-router-dom'
+import Map from './Map'
 
 interface Istore {
-store: {}
-posts: Array<ITask>
+  store: {}
+  posts: Array<ITask>
 }
 
 export default function FindTask() {
-    const tasks = useSelector((store:Istore)=> store.posts)
-    const dispatch = useDispatch();
-   
-    const [state, setState] = useState({
-        frst: true,
-        scnd: false,
-        thrd: false,
-      });
+  const tasks = useSelector((store: Istore) => store.posts)
+  const dispatch = useDispatch();
 
-      const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setState({
-          ...state,
-          [event.target.name]: event.target.checked,
-        });
-      };
-      const { frst, scnd, thrd } = state;
-   
-    useEffect(()=>{
-        dispatch(fetchPosts(state))
-    },[state])
+  const [state, setState] = useState({
+    frst: true,
+    scnd: false,
+    thrd: false,
+    fourth: false,
+    fifth: false,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
+  };
+  const { frst, scnd, thrd, fourth, fifth } = state;
+
+  useEffect(() => {
+    dispatch(fetchPosts(state))
+  }, [state])
+
   return (
     <div>
-    <div>
-       {tasks.map((el) => <OneTask key={el.id} el={el}/>)} 
-    </div>
-    <div>
-    <FormLabel component="legend">Chose category</FormLabel>
-        <FormGroup>
+      <div>
+        <FormLabel style={{ margin: '10px' }} component="legend"><h3>Выберите категорию</h3></FormLabel>
+        <FormGroup >
           <FormControlLabel
             control={
               <Checkbox checked={frst} onChange={handleChange} name="frst" />
             }
-            label="frst category"
+            label="Бытовые услуги"
           />
           <FormControlLabel
             control={
               <Checkbox checked={scnd} onChange={handleChange} name="scnd" />
             }
-            label="scnd category "
+            label="Грузоперевозки"
           />
           <FormControlLabel
             control={
               <Checkbox checked={thrd} onChange={handleChange} name="thrd" />
             }
-            label="thrd category"
+            label="Курьерские услуги"
           />
+          <FormControlLabel
+            control={
+              <Checkbox checked={fourth} onChange={handleChange} name="fourth" />
+            }
+            label="Компьютерная помощь"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={fifth} onChange={handleChange} name="fifth" />
+            }
+            label="Красота и здоровье"
+          />
+          <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+            {tasks.map((el) => <OneTask key={el.id} el={el} />)}
+          </div>
         </FormGroup>
-    </div>
+      </div>
+      <br />
+      <Map />
     </div>
   )
 }
