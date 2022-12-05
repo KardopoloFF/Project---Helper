c52b82ce4b4f168d00856ef765664da13a195fbe
Aleksandr Proshkov <all9100@mail.ru>
2022-12-05 18:38:20 +0300

    fix v 229

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -3,6 +3,7 @@ import createSagaMiddleware from 'redux-saga';
 import setPostsReducer from './slices/postsSlice';
 import setUserReducer from './slices/userSlice'
 import setCategories from './slices/categoriesSlice';
+import displayedGeoobjectsReducer from './slices/displayedGeoobjectsSlice';
 import postsSaga from './sagas/posts';
 import categoriesSaga from './sagas/categories'
 import setOnePostReducer from './slices/onePostSlice'
@@ -13,9 +14,10 @@ export default configureStore({
     posts: setPostsReducer,
     user: setUserReducer,
     categories: setCategories,
+    displayedGeoobjects: displayedGeoobjectsReducer,
     onePost: setOnePostReducer,
   },
   middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), sagaMiddleware],
-});
+})
 sagaMiddleware.run(postsSaga);
 sagaMiddleware.run(categoriesSaga);
