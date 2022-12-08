88d6cedc84250643bbf0a0cf4c746eced6675c82
Nikita Kardopolov <reverdj@yandex.ru>
2022-12-08 11:55:22 +0300

    new seeds

--- server/db/seeders/20221201140718-category.js
+++ server/db/seeders/20221201140718-category.js
@@ -3,34 +3,36 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Categories', [{
+    await queryInterface.bulkInsert('Categories', [
+    {
       name: 'Бытовые услуги',
       createdAt: new Date(),
       updatedAt: new Date(),
-    }], {});
-    await queryInterface.bulkInsert('Categories', [{
+    },
+    {
       name: 'Грузоперевозки',
       createdAt: new Date(),
       updatedAt: new Date(),
-    }], {});
-    await queryInterface.bulkInsert('Categories', [{
+    },
+    {
       name: 'Курьерские услуги',
       createdAt: new Date(),
       updatedAt: new Date(),
-    }], {});
-    await queryInterface.bulkInsert('Categories', [{
+    },
+    {
       name: 'Компьютерная помощь',
       createdAt: new Date(),
       updatedAt: new Date(),
-    }], {});
-    await queryInterface.bulkInsert('Categories', [{
+    },
+    {
       name: 'Красота и здоровье',
       createdAt: new Date(),
       updatedAt: new Date(),
-    }], {});
-  },
+    },
+  ], {});
+},
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Category', null, {});
+    await queryInterface.bulkDelete('Categories', null, {});
   },
 };
